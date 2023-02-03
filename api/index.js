d2f962465e4b21e9046196f27c3a559b8b41b5d8
Juan Jure <jpjure04@gmail.com>
2023-02-03 08:10:41 -0500

    Api with html

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json({
 app.use(express.static(path.join(__dirname, '/../web')));
 
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     //Envia index.html a la solicitud
     res.sendFile(path.join(__dirname, '/../web', 'index.html'));
 })
