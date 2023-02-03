f77bbb7b76a157d52f5dcbac4dfcc9b5d72a6f94
Juan Jure <jpjure04@gmail.com>
2023-02-03 09:34:13 -0500

    vercel

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json({
 app.use(express.static(path.join(__dirname, '/../web')));
 
 
-app.get('/',(req,res)=>{
+app.get('/api',(req,res)=>{
     //Envia index.html a la solicitud
     res.sendFile(path.join(__dirname, '/../web', 'index.html'));
 })
