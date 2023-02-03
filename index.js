65d6e78585a682f0c80dbdf561cd34106988a1a7
Juan Jure <jpjure04@gmail.com>
2023-02-03 14:28:09 -0500

    vercel

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path');
+const router = express.Router();
+const app = express()
+
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json({
+    type: "*/*"
+}))
+
+
+app.use(express.static('web'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')})
+})
+
+app.get('/api',(req,res)=>{
+    //Envia index.html a la solicitud
+    console.log("estas en api")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
