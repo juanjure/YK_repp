d31b5508814c64810c43acc110cbe1a658012f0d
Juan Jure <jpjure04@gmail.com>
2023-02-03 00:23:49 -0500

    patico

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
+
 
 app.use(
     express.urlencoded({
@@ -13,8 +15,14 @@ app.use(express.json({
     type: "*/*"
 }))
 
-app.get('/api', (req, res)=>{
-    res.send("Hola mundo")
+
+app.use(express.static(path.join(__dirname, '/../web')));
+
+
+app.get('/home',(req,res)=>{
+    //Envia index.html a la solicitud
+    res.sendFile(path.join(__dirname, '/../web', 'index.html'));
 })
+
 
 app.listen(port)
