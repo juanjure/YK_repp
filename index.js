88e5e61826cfaa7fd6596d8cb7ab0707e2ee0a19
Juan Jure <jpjure04@gmail.com>
2023-02-03 17:21:11 -0500

    data base

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const pool = new Pool({
     ssl: {
       rejectUnauthorized: false
     }
-});
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -24,12 +24,81 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')})
 })
 
-app.get(
-    '/api',
-    (request, response) => {
-        response.send('pagina Principal')
+app.post('/create', async (req, res) => {
+    const {TIEMPO, TEMPERATURA, HUMEDAD, LUZ, PUMP} = req.body;
+    const client = await pool.connect();
+    try {
+      await client.query(`INSERT INTO mds (TIEMPO, TEMPERATURA, HUMEDAD, LUZ, PUMP) VALUES('${TIEMPO}', ${TEMPERATURA} , ${HUMEDAD} , ${LUZ} , '${PUMP}')`);
+      res.send('NUEVOS DATOS CREADOS');
+    } finally {
+      client.release();
     }
-);
+  });
+  
+  app.get('/read', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const { rows } = await client.query('SELECT * FROM mds s');
+      res.send(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error reading from database' });
+    } finally {
+      client.release();
+    }
+  });
+  
+  app.get('/read_last_20', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const { rows } = await client.query('SELECT * FROM mds ORDER BY id DESC LIMIT 20');
+      res.send(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error reading from database' });
+    } finally {
+      client.release();
+    }
+  });
+  
+  app.get('/lumi', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const { rows } = await client.query('SELECT LUZ FROM MDS ORDER BY id DESC LIMIT 1');
+      res.send(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error reading from database' });
+    } finally {
+      client.release();
+    }
+  });
+  
+  app.get('/temp', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const { rows } = await client.query('SELECT TEMPERATURA FROM MDS ORDER BY id DESC LIMIT 1');
+      res.send(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error reading from database' });
+    } finally {
+      client.release();
+    }
+  });
+  
+  app.get('/hume', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const { rows } = await client.query('SELECT HUMEDAD FROM MDS ORDER BY id DESC LIMIT 1');
+      res.send(rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'Error reading from database' });
+    } finally {
+      client.release();
+    }
+  });
 
 
 
