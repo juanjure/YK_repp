b7da575051beb4f32987c4d31d9068ec93036803
Juan Jure <jpjure04@gmail.com>
2023-02-02 20:59:24 -0500

    Clean cache

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(
     express.urlencoded({
@@ -17,6 +17,5 @@ app.get('/', (req, res)=>{
     res.send("Hola mundo")
 })
 
-app.listen(port, () => {
-    console.log(`Estoy ejecutandome en http://localhost:${port}`)
-})
+app.listen(port)
+
